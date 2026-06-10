518421e38ad28f108d70796c703c5e54232813bd
HushV1.7 <shinspections55@gmail.com>
2026-06-10 18:53:44 -0500

    Configure Firebase project keys

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,9 @@
 window.HUSH_FIREBASE_CONFIG = {
-  apiKey: 'REPLACE_WITH_FIREBASE_API_KEY',
-  authDomain: 'REPLACE_WITH_FIREBASE_AUTH_DOMAIN',
-  projectId: 'REPLACE_WITH_FIREBASE_PROJECT_ID',
-  appId: 'REPLACE_WITH_FIREBASE_APP_ID'
+  apiKey: 'AIzaSyBG2Dz64IjL5_ObkL7GSxN3WaajwNu6wAE',
+  authDomain: 'hushff-81612.firebaseapp.com',
+  projectId: 'hushff-81612',
+  storageBucket: 'hushff-81612.firebasestorage.app',
+  messagingSenderId: '285459097708',
+  appId: '1:285459097708:web:7e03f5c9e687db8be61049',
+  measurementId: 'G-4BYC1RY7TW'
 };
